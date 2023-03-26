c410009f7ba52a09557aaa5269e8ffc751b96382
mardon1dev <dusbekovmardonbek5430@gmail.com>
2023-03-26 16:10:45 +0500

    ok

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-const search = document.querySelector(".search");
-const toggledInput = document.querySelector(".input");
-const btn = document.querySelector(".btn");
-const body = document.querySelector(".body1");
-btn.addEventListener("click", ()=>{
-    search.classList.toggle("active");
-    toggledInput.focus();  
-    body.classList.toggle("white")
-    search.value = "";
-})
+const ism = document.querySelector("h2");
+const put = prompt("Ismingiz");
+
+
+
+ism.textContent = put;
